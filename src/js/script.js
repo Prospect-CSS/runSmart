46900587a942c1408b8e20c1e3d22fb0609fa420
Aleksei <agsfinks@gmail.com>
2022-11-20 23:37:29 +0000

    added tabs section and carusej section

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.carusel__wrapper').slick({
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 5000,
         speed: 1200,
         adaptiveHeight: true,
@@ -9,4 +9,26 @@ $(document).ready(function () {
         nextArrow: '<button type="button" class="slick-prev carusel__next"></button>',
 
     });
+
+
+
+
+    $('.catalog__tabs').on('click', 'li:not(.catalog__tab--active)', function () {
+        $(this)
+            .addClass('catalog__tab--active').siblings().removeClass('catalog__tab--active')
+            .closest('.container952').find('.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
+    });
+
+    toggleActiveClass('.card-catalog__frontflip');
+    toggleActiveClass('.card-catalog__backflip');
+
+    function toggleActiveClass(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.card-catalog__frontflip').eq(i).toggleClass('card-catalog__frontflip--active');
+                $('.card-catalog__backflip').eq(i).toggleClass('card-catalog__backflip--active');
+            });
+        });
+    }
 });
